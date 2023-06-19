131c41183852b93ec89df3b9ebb241a2e9343021
hernanysimao <hernanysimao123@gmail.com>
2023-06-19 17:06:40 +0100

    adicionando config build

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,9 +45,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    distDir: "dist",
+    distDir: "./distGenerate",
   },
   generate: {
-    dir: "dist",
+    dir: "./distGenerate",
   },
 };
